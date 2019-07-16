995298fdfb6cced85a08ea64c70c89c5ded3c83a
plucinskith <thomas.plucinski@sunzinet.com>
2019-07-16 17:08:14 +0200

    Add further styling to buttons header, adjust Grund routines, clean the JS-file

--- js/main.js
+++ js/main.js
@@ -12,80 +12,164 @@
  */
 let someModule = (function(){
 
-    //------------  Definition of HTML node elements (private) ------------------//
 
-    let $moduleNode = document.querySelector('.module');
+    let Constructor = function(){
+
+        //------------  Definition of HTML node elements (private) ------------------//
+
+        let $moduleNode = document.querySelector('.module');
 
 
-    //check if node element is available
-    //If not exit the creation of the module - return of init: false indicates this
-    if (!$moduleNode){
-        return {
-            init: (function(accordeon){return false})
+        //check if node element is available
+        //If not exit the creation of the module - return of init: false indicates this
+        if (!$moduleNode){
+            return {
+                init: (function(accordeon){return false})
+            }
         }
-    }
 
-    let $listItems = $moduleNode.querySelectorAll('.module__list-item');
-    let $menuIcon = $moduleNode.querySelector('.module__icon');
-    let $firstLevelItems = $moduleNode.querySelectorAll('.module__list-item>li>a');
+        let $listItems = $moduleNode.querySelectorAll('.module__list-item');
+        let $menuIcon = $moduleNode.querySelector('.module__icon');
+        let $firstLevelItems = $moduleNode.querySelectorAll('.module__list-item>li>a');
 
-    //------------  Definition of variables ---------------------------//
+        //------------  Definition of variables ---------------------------//
 
-    let isSomeBoolean = false;
+        let isSomeBoolean = false;
 
-    //-------------- Definition of private functions ---------------------------//
+        //-------------- Definition of private functions ---------------------------//
 
-    function initEvents(){
+        function initEvents(){
 
 
-        //Click events
-        $moduleNode.addEventListener('click', function(event) {
-           if(event.target && event.target.matches('a')){
-               //Add Actions, call Handlers
-               someClickHandler();
-           }
-        });
+            //Click events
+            $moduleNode.addEventListener('click', function(event) {
+               if(event.target && event.target.matches('a')){
+                   //Add Actions, call Handlers
+                   someClickHandler();
+               }
+            });
 
-        //Wait for the browser to fully render the page
-        window.addEventListener('load', function() {
-           //Add on page load handlers
-            someOnloadHandler();
-        });
-    }
+            //Wait for the browser to fully render the page
+            window.addEventListener('load', function() {
+               //Add on page load handlers
+                someOnloadHandler();
+            });
+        }
 
-    function someOnloadHandler() {
-        //Add handler actions
-    }
+        function someOnloadHandler() {
+            //Add handler actions
+        }
 
-    function someClickHandler(){
-        //Add click handler actions
-        someOtherFunction();
-    }
+        function someClickHandler(){
+            //Add click handler actions
+            someOtherFunction();
+        }
 
-    function someOtherFunction(){
-        //Some actions
-    }
+        function someOtherFunction(){
+            //Some actions
+        }
 
-    //Init actions
-    function initMenu(someParam) {
+        //Init actions
+        function initMenu(someParam) {
 
-        //Pass parameters
-        isSomeBoolean = someParam;
+            //Pass parameters
+            isSomeBoolean = someParam;
 
-        initEvents();
-    }
+            initEvents();
+        }
 
-    //------------------- Call of init functions ------------------------------//
-
+        //------------------- Call of init functions ------------------------------//
 
 
 
-    //-------------- Definition of public functions ---------------------------//
-    return {
-        init: initMenu
-    }
+
+        //-------------- Definition of public functions ---------------------------//
+        return {
+            init: initMenu
+        }
+    };
+
+    return Constructor;
+
 
 })();
+
+/**
+ * Module for floating labels
+ * @type {{init}}
+ */
+let floatLabels = (function(){
+
+    let Constructor = function(floatLabelClass){
+
+        //------------  Definition of HTML node elements (private) ------------------//
+
+        let $floatLabels = [];
+        let $floatLabelInputs = [];
+        let $floatLabelGroups = [];
+
+
+        //-------------- Definition of private functions ---------------------------//
+
+        function initEvents(){
+
+
+            for (let i=0; i < $floatLabelInputs.length; i++) {
+
+                if($floatLabelInputs[i].value && $floatLabelInputs[i].value !== '') {
+                    $floatLabelInputs[i].parentNode.classList.add('filled');
+                }
+                $floatLabelInputs[i].addEventListener('focus', focusHandler);
+                $floatLabelInputs[i].addEventListener('focusout', focusOutHandler)
+            }
+        }
+
+        function focusHandler(e){
+            e.target.parentNode.classList.add('active');
+        }
+
+        function focusOutHandler(e){
+            e.target.parentNode.classList.remove('active');
+            if (e.target.value && e.target.value !== '') {
+                e.target.parentNode.classList.add('filled');
+            }else {
+                e.target.parentNode.classList.remove('filled');
+            }
+        }
+
+        //Init actions
+        function init(floatLabelClass) {
+
+            //Pass parameters
+            $floatLabels = document.querySelectorAll('.' + floatLabelClass + ', ' + floatLabelClass);
+
+            for (let i=0; i < $floatLabels.length; i++) {
+
+                let $group = $floatLabels[i].parentNode;
+                let $input = $group.querySelector('input');
+                $floatLabelGroups.push($group);
+                $floatLabelInputs.push($input);
+            }
+
+            initEvents();
+        }
+
+        //------------------- Call of init functions ------------------------------//
+
+        init(floatLabelClass);
+
+
+        //-------------- Definition of public functions ---------------------------//
+        return {
+            init: init
+        }
+    };
+
+    return Constructor;
+
+})();
+
+
 
 
 
@@ -112,7 +196,9 @@ let someModule = (function(){
     }
 
 
-    someModule.init(false);
+    new someModule();
+
+    new floatLabels('form__float-label');
 
 })();
 
